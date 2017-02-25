64944b9892065c7a3b95c39420db215e00159839
Diego Goya <diego.goya@gmail.com>
2017-02-25 16:17:04 +0100

    before-tasks

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
 	belongs_to :user
 	has_many :users, through: :user_events
 	has_many :user_events
+
+        has_many :tasks
+
+        accepts_nested_attributes_for :tasks
 end
