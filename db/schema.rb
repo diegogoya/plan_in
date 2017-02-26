a8a6818d79ec8ee5b031b4b56639520e3f6da4f5
Diego Goya <diego.goya@gmail.com>
2017-02-26 02:11:17 +0100

    registration-finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223202347) do
+ActiveRecord::Schema.define(version: 20170225151759) do
 
   create_table "choices", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,12 @@ ActiveRecord::Schema.define(version: 20170223202347) do
     t.datetime "updated_at",             null: false
     t.index ["event_id"], name: "index_registrations_on_event_id"
     t.index ["user_id"], name: "index_registrations_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
